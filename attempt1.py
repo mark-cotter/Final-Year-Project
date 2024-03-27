139ac4b2d755799d938c12cd1bacfe9013f51d70
mark-cotter <160327409+mark-cotter@users.noreply.github.com>
2024-03-27 17:57:33 +0000

    Update attempt1.py

--- attempt1.py
+++ attempt1.py
@@ -581,7 +581,7 @@ def Q2Q_heatmap(df_data):
     st.pyplot(fig)
 
 def full_data_heatmap(df_data):
-    columns_of_interest = ["Disney+ Subscribers","Netflix Subscribers","Peacock Subscribers","Hulu Subscribers"]
+    columns_of_interest = ["Disney+ Subscribers","Netflix Subscribers","Peacock Subscribers","Hulu Subscribers","Disney Sub Change Q2Q", "Netflix Sub Change Q2Q", "Hulu Sub Change Q2Q", "Peacock Sub Change Q2Q"]
     subset = df_data[columns_of_interest]
     correlation_matrix = subset.corr()
     fig, ax = plt.subplots(figsize=(8, 6))
