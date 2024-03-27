504ccc93c703dc080f2945ce460d78089538c0a6
mark-cotter <160327409+mark-cotter@users.noreply.github.com>
2024-03-27 17:11:21 +0000

    Update attempt1.py

--- attempt1.py
+++ attempt1.py
@@ -574,7 +574,6 @@ def generate_correlation_heatmap(df_data):
     columns_of_interest = ["Disney Sub Change Q2Q", "Netflix Sub Change Q2Q", "Hulu Sub Change Q2Q", "Peacock Sub Change Q2Q"]
     subset = df_data[columns_of_interest]
     correlation_matrix = subset.corr()
-    plt.figure(figsize=(8, 6))
     sns.heatmap(correlation_matrix, annot=True, cmap='viridis', fmt=".2f")
     plt.title('Correlation Matrix Heatmap')
     
